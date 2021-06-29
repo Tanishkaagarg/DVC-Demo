09e70cef8e4374414cea85ac3e04ca1fe3d3a652
garg-tanishka <tanishka.garg@knoldus.com>
2021-06-29 15:46:42 +0530

    First Stage completed

--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,7 @@
-
 ## read params
 ## process
 ## return dataframe
+
 import os
 import yaml
 import pandas as pd
@@ -14,11 +14,11 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    # print(config)
+    #print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    print(df.head())
-    #return df
+    #print(df.head())
+    return df
 
 
 
